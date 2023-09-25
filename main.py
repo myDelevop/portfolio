105a3b62997ab0a813c0b1f56bb2444589fbe2ea
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-25 15:30:21 +0200

    Implemented business logic for send email and save contact form on DB

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import os
+import smtplib
+
 from flask_bootstrap import  Bootstrap5
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, EmailField, TelField, TextAreaField
 from wtforms.validators import DataRequired
 
+email_to = os.getenv("EMAIL_TO")
+email_login = os.getenv("EMAIL_FROM")
+email_login_psw = os.getenv("EMAIL_FROM_psw")
+
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 app.config['SECRET_KEY'] = os.getenv("PORTFOLIO_SECRET_KEY")
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    name = db.Column(db.String(25), nullable=False)
+    surname = db.Column(db.String(25), nullable=False)
+    number = db.Column(db.String(15), nullable=False)
+    message = db.Column(db.String(450), nullable=False)
 
 
 class ContactForm(FlaskForm):
@@ -17,6 +34,10 @@ class ContactForm(FlaskForm):
     number = TelField('Number', validators=[DataRequired()])
     message = TextAreaField('Message', validators=[DataRequired()])
     submit = SubmitField('SEND')
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
@@ -39,6 +60,23 @@ def contact():
         number = request.form.get("number")
         message = request.form.get("message")
 
+        con = Contact()
+        con.name = name
+        con.surname = surname
+        con.email = email
+        con.number = number
+        con.message = message
+
+        db.session.add(con)
+        db.session.commit()
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=email_login, password=email_login_psw)
+
+            connection.sendmail(from_addr=email,
+                                to_addrs="rocco.caliandro@toptal.com",
+                                msg=message)
         return render_template("index.html", form_complete=True)
 
     else:
