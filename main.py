078a0c641ece35e45fb0a30d57bbc2b63d1abab9
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-26 18:35:31 +0200

    tried another method to send email

--- main.py
+++ main.py
@@ -76,11 +76,34 @@ def contact():
         app.debug = True
         db.session.add(con)
         db.session.commit()
-        app.debug = False
 
+        smtp_server = "smtp.gmail.com"
+        port = 587  # For starttls
+        sender_email = "rockdesires@gmail.com"
+        password = email_login_psw
+
+        # Create a secure SSL context
+        context = ssl.create_default_context()
+
+        # Try to log in to server and send email
+        try:
+            server = smtplib.SMTP(smtp_server, port)
+            server.ehlo()  # Can be omitted
+            server.starttls(context=context)  # Secure the connection
+            server.ehlo()  # Can be omitted
+            server.login(sender_email, password)
+            # TODO: Send email here
+        except Exception as e:
+            # Print any error messages to stdout
+            return render_template("index.html", form_complete=0)
+        finally:
+            server.quit()
+
+
+        app.debug = False
         # try:
-        with smtplib.SMTP("smtp.gmail.com") as connection:
-            print("CIZO")
+        #with smtplib.SMTP("smtp.gmail.com") as connection:
+            #print("CIZO")
             # connection.starttls()
             # connection.login(user=email_login, password=email_login_psw)
             # connection.sendmail(from_addr=email_login,
