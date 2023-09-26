858a22b8e30a7c280943d358860e32923864db9b
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-09-26 09:11:40 +0200

    added name for the table

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ db.init_app(app)
 
 
 class Contact(db.Model):
+    __tablename__ = "contacts"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), nullable=False)
     name = db.Column(db.String(25), nullable=False)
